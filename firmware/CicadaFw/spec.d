6b6fbe39ffb57f371d38db4d06f15e4a8381e197
mars2860 <anton.sysoev.ru68@gmail.com>
2024-05-26 19:29:47 +0300

    Updated firmware project files

--- firmware/CicadaFw/spec.d
+++ firmware/CicadaFw/spec.d
@@ -1,2 +1,4 @@
 spec.o: \
- D:/Projects/Neon/.metadata/.plugins/org.eclipse.cdt.managedbuilder.core/spec.C
+ D:/Projects/Neon/.metadata/.plugins/org.eclipse.cdt.managedbuilder.core/spec.C \
+ D:\SloeberIDE\arduinoPlugin\packages\esp8266\hardware\esp8266\3.1.2\cores\esp8266\CommonHFile.h \
+ D:\Projects\Devices\MyCopter\firmware\CicadaFw\ESP-12E\core\CicadaFw.globals.h
